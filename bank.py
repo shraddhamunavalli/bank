0278c247befac29616973354688b1d8c09261d1e
shraddhamunavalli <munavallishraddha4@gmail.com>
2025-11-21 11:14:07 +0530

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,13 +1,13 @@
 import sys
 
 if len(sys.argv) == 3:
-    balance = sys.argv[1]
-    deposit = sys.argv[2]
+    current= sys.argv[1]
+    withdrawal = sys.argv[2]
     print("User provided input values:")
 else:
     print("No input given - using default values:")
-    balance = "1000"
-    deposit = "500"
+    current = "1000"
+    withdrawal= "500"
 
-updated_balance = eval(balance) + eval(deposit)
+updated_balance = eval(current) -eval(withdrawal)
 print("Updated Balance:", updated_balance)
